249441c238f886bbb2029a5c2e04a34d8bf98fb8
samanth2975 <99540713+samanth2975@users.noreply.github.com>
2022-06-24 13:06:41 +0530

    Delete Streams.js

--- Streams.js
+++
@@ -1,9 +0,0 @@
-var http= require('http');
-var fs = require('fs');
-
-var myReadStreams = fs.createReadStream(__dirname + '/readMe.txt');
- 
-myReadStreams.on('data',function(chunk){
-    console.log('new chunk received:');
-    console.log(chunk);
-});
